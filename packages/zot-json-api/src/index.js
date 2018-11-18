2d04574594697ca62d3f35400ac786ca8bb1e503
Adam Henson <adamhenson1979@gmail.com>
2018-11-17 23:47:03 -0500

    jsonapi-server

---
+++ packages/zot-json-api/src/index.js
@@ -0,0 +1,19 @@
+const jsonApi = require("jsonapi-server");
+
+jsonApi.setConfig({
+  port: 16006,
+  graphiql: true
+});
+
+jsonApi.define({
+  resource: 'photos',
+  handlers: new jsonApi.MemoryHandler(),
+  attributes: {
+    title: jsonApi.Joi.string(),
+    url: jsonApi.Joi.string().uri(),
+    height: jsonApi.Joi.number().min(1).max(10000).precision(0),
+    width: jsonApi.Joi.number().min(1).max(10000).precision(0)
+  }
+});
+
+jsonApi.start();
